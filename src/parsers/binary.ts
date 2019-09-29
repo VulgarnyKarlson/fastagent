adb13c450cf18a616b41291b14e6a44e0b76fa24
VulgarnyKarlson <vulgarnykarlson@gmail.com>
2019-09-29 21:53:46 +0300

    Improve binary concat

--- src/parsers/binary.ts
+++ src/parsers/binary.ts
@@ -2,9 +2,9 @@ export default (res, fn) => {
     const data = []; // Binary data needs binary storage
 
     res.on('data', chunk => {
-        data.push(chunk);
+        data.push(...chunk);
     });
     res.on('end', () => {
-        fn(null, Buffer.concat(data));
+        fn(null, Buffer.from(data));
     });
 };
