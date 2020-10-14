dace864d8e797bfce7530e765d9ee1bed2bddf17
Ibragim Zubailov <ibragim.zubailov@onlinesup.com>
2020-10-14 19:29:52 +0300

    remove unnecasery console.log

--- src/res/parsers/json.ts
+++ src/res/parsers/json.ts
@@ -1,6 +1,5 @@
 export default (data: Buffer): Buffer | object  => {
     try {
-        console.log(data.toString("utf8"));
         return JSON.parse(data.toString("utf8"));
     } catch (err2) {
         return data;
